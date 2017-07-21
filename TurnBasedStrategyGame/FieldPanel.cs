8d8df55b2f838046003551ca54ca63eaf404d65d
Simo Muraja <simo.muraja@aalto.fi>
2017-07-21 16:12:32 +0300

    Add Fieldpanel

---
+++ TurnBasedStrategyGame/FieldPanel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TBSG
+{
+    public class FieldPanel : PictureBox
+    {
+        protected override void OnMouseHover(System.EventArgs e)
+        {
+            base.OnMouseHover(e);
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+        }
+    }
+}
