3e000b40fc5109ecd66b974934633408638e07f5
Simo Muraja <simo.muraja@aalto.fi>
2017-07-23 12:41:45 +0300

    Fix default namespace

--- TurnBasedStrategyGame/ConfigurationProvider.cs
+++ TurnBasedStrategyGame/ConfigurationProvider.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TurnBasedStrategyGame.Properties;
-
-namespace TBSG
+﻿namespace TBSG
 {
     public class ConfigurationProvider : IConfigurationProvider
     {
         public T GetValue<T>(string key)
         {
-            return (T)Settings.Default[key];
+            return (T)Properties.Settings.Default[key];
         }
     }
 }
